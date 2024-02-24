6fe7db24faf1cf8ff0582d826e29a692547c8f0e
codeur974 <eric.sermande@gmail.com>
2024-02-24 11:51:55 +0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="cours react" description="description.." />
+      <h1>Hello world</h1>
+    </>
+  );
+}
