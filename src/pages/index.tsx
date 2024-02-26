b2af7b281bfbfe81e48ff7e67df023e9258a2d67
codeur974 <eric.sermande@gmail.com>
2024-02-26 13:21:33 +0400

    création composant bouton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import { Seo } from "@/ui/components/seo";
 import { Typography } from "@/ui/desin-system/typography/typography";
-
+import { Button } from "@/ui/desin-system/button/button";
 export default function Home() {
   return (
     <>
-      <Seo title="cours react" description="description.." />
+      <Seo title="cour-react" description="Description..."></Seo>
+
+      <div className="flex items-center gap-4 p-10">
+        <Button size="small" variant="accent">
+          Button
+        </Button>
+        <Button size="medium" variant="secondary">
+          bonjour
+        </Button>
+        <Button size="large" variant="outline">
+          Celia
+        </Button>
+        <Button size="medium" variant="disabled">
+          Patou
+        </Button>
+      </div>
 
       <div className="space-y-5">
         <Typography theme="primary" variant="display" component="div">
